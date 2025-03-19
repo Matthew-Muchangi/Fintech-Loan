edb0125b4e518c0dc98f8089c974a4f53038f967
Matthew <matthewmuchangi@gmail.com>
2025-03-19 12:31:04 +0300

    initial commit

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+export interface Customer {
+  id: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  dateOfRegistration: string;
+  customerType: string;
+}
+
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
@@ -8,49 +17,69 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CustomerComponent implements OnInit {
   customerForm!: FormGroup;
-  customers: any[] = [];
-  displayedColumns: string[] = ['name', 'email', 'phone', 'customerType', 'actions'];
+  customers: Customer[] = [];
+  lastid: string | null = null;
+  editMode: boolean = false;
+  editedCustomerIndex: number | null = null; // Index of customer being edited
 
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-    this.initForm();
-    this.loadCustomers();
-  }
-
-  initForm() {
+  ngOnInit() {
     this.customerForm = this.fb.group({
       fullName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.required],
-      customerType: ['', Validators.required],
-      registrationDate: ['', Validators.required]
+      dateOfRegistration: ['', Validators.required],
+      customerType: ['', Validators.required]
     });
+
+    this.loadCustomers();
+  }
+
+  generateid(): string {
+    return 'CUST-' + Math.floor(1000 + Math.random() * 9000);
   }
 
   saveCustomer() {
     if (this.customerForm.valid) {
-      const newCustomer = this.customerForm.value;
-      this.customers.push(newCustomer);
+      if (this.editMode && this.editedCustomerIndex !== null) {
+        // Update existing customer
+        this.customers[this.editedCustomerIndex] = {
+          ...this.customers[this.editedCustomerIndex],
+          ...this.customerForm.value,
+        };
+        this.editMode = false;
+        this.editedCustomerIndex = null;
+      } else {
+        // Create a new customer
+        const newCustomer: Customer = {
+          id: this.generateid(),
+          ...this.customerForm.value
+        };
+
+        this.customers.push(newCustomer);
+        this.lastid = newCustomer.id;
+      }
+
       localStorage.setItem('customers', JSON.stringify(this.customers));
       this.customerForm.reset();
     }
   }
 
   loadCustomers() {
-    const storedCustomers = localStorage.getItem('customers');
-    if (storedCustomers) {
-      this.customers = JSON.parse(storedCustomers);
-    }
+    const savedCustomers = localStorage.getItem('customers');
+    this.customers = savedCustomers ? JSON.parse(savedCustomers) : [];
   }
 
-  deleteCustomer(customer: any) {
-    this.customers = this.customers.filter(c => c !== customer);
-    localStorage.setItem('customers', JSON.stringify(this.customers));
-  }
-
-  editCustomer(customer: any) {
+  editCustomer(index: number) {
+    const customer = this.customers[index];
     this.customerForm.patchValue(customer);
-    this.deleteCustomer(customer);
+    this.editMode = true;
+    this.editedCustomerIndex = index;
+  }
+
+  deleteCustomer(index: number) {
+    this.customers.splice(index, 1);
+    localStorage.setItem('customers', JSON.stringify(this.customers));
   }
 }
